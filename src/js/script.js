3f0f9f94cef3eb438578c2ea3fcf1c8fa4d48624
Danila <danil.rsupe@yandex.ru>
2020-04-06 14:27:10 +0300

    added feedback and inner

--- src/js/script.js
+++ src/js/script.js
@@ -23,3 +23,22 @@ validateForms('#call-center');
 jQuery(function($){
     $('input[name=phone]').mask("+7 (999) 999-99-99");
 });
+
+$(document).ready(function(){
+    $('.inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icon/arrow_left.png"</button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icon/arrow_right.png"</button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    dotsClass: 'dots-style',
+                    arrows: false
+                }
+            }
+        ]
+    });
+});
